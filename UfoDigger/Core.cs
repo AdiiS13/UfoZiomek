17bd15eca66701309fd3ac129ac580bda9901409
Michał Mlostek <mmlostek@st.swps.edu.pl>
2025-06-06 19:41:34 +0200

    Add project files.

---
+++ UfoDigger/Core.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UfoDigger
+{
+    public static class Core
+    {
+        //przypisywanie klawiszy
+        public readonly static Keys KeyUp = Keys.W;
+        public readonly static Keys KeyDown = Keys.S;
+        public readonly static Keys KeyLeft = Keys.A;
+        public readonly static Keys KeyRight = Keys.D;
+
+        //bazowa wartosc klawisza
+        public static bool IsUp = false;
+        public static bool IsDown = false;
+        public static bool IsLeft = false;
+        public static bool IsRight = false;
+    }
+}
---
+++ UfoDigger/GameObjects/Player.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UfoDigger.GameObjects
+{
+    public class Player : UserControl
+    {
+        private Timer timerUpdate;
+        private System.ComponentModel.IContainer components;
+        private PictureBox pictureBox1;
+
+        public int speed = 5;
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::UfoDigger.Properties.Resources.alien_bubble;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timerUpdate
+            // 
+            this.timerUpdate.Enabled = true;
+            this.timerUpdate.Interval = 25;
+            this.timerUpdate.Tick += new System.EventHandler(this.Update);
+            // 
+            // Player
+            // 
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Player";
+            this.Size = new System.Drawing.Size(64, 64);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        public Player()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            //aktualizowanie pozycji gracza
+            if (Core.IsUp)
+                Top -= speed;
+            if (Core.IsDown)
+                Top += speed;
+            if (Core.IsLeft)
+                Left -= speed;
+            if (Core.IsRight)
+                Left += speed;
+        }
+    }
+}
