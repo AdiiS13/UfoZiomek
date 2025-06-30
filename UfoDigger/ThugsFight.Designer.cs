18d3d8ccf7d4e6c1f099b0c1c36c25d7594a3f40
MatG3b17 <mgebka@st.swps.edu.pl>
2025-06-30 20:32:40 +0200

    Upiększenie mini gierki ThugsFight
    +Dodanie gry papier, kamień i nożyce do okienka "ThugsFight"
    +Dodanie do zasobów nowe obrazków dla wspomnianej mini gierki
    *Trzeba dodać mechanikę stracenia pieniędzy/paliwa po przegranej

--- UfoDigger/ThugsFight.Designer.cs
+++ UfoDigger/ThugsFight.Designer.cs
@@ -28,20 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::UfoDigger.Properties.Resources.kamienD;
+            this.pictureBox1.InitialImage = global::UfoDigger.Properties.Resources.kamienD;
+            this.pictureBox1.Location = new System.Drawing.Point(393, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 198);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::UfoDigger.Properties.Resources.kamienU;
+            this.pictureBox2.InitialImage = global::UfoDigger.Properties.Resources.kamienU;
+            this.pictureBox2.Location = new System.Drawing.Point(87, 81);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(210, 198);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label1.Location = new System.Drawing.Point(87, 282);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ufoludek";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tempus Sans ITC", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label2.Location = new System.Drawing.Point(393, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dresiarz";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Liberation Mono", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(664, 43);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Papier Kamień Nożyce";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Niagara Engraved", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(253, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 53);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Zagraj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // ThugsFight
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(688, 410);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ThugsFight";
             this.Text = "ThugsFight";
             this.Load += new System.EventHandler(this.ThugsFight_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
--- UfoDigger/ThugsFight.cs
+++ UfoDigger/ThugsFight.cs
@@ -10,8 +10,10 @@ using System.Windows.Forms;
 
 namespace UfoDigger
 {
+
     public partial class ThugsFight : Form
     {
+        Random random = new Random();
         public ThugsFight()
         {
             InitializeComponent();
@@ -20,6 +22,78 @@ namespace UfoDigger
         private void ThugsFight_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void RPSLost() ///Usuniencie punktów + benzyny po przegranien do dodaniaAdd commentMore actions
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int computerTurn = random.Next(1, 4);
+            int playerTurn = random.Next(1, 4);
+
+            ///Obrazków dla renki ufoludka
+            switch (playerTurn)
+            {
+                case 1:
+                    pictureBox2.Image = Properties.Resources.kamienU;
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 2:
+                    pictureBox2.Image = Properties.Resources.papierU;
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 3:
+                    pictureBox2.Image = Properties.Resources.nozyceU;
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                default:
+                    pictureBox2.Image = Properties.Resources.kamienU;
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+            }
+            ///Obrazki dla renki dresiarza
+            switch (computerTurn)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.kamienD;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.papierD;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.nozyceD;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+                default:
+                    pictureBox1.Image = Properties.Resources.kamienD;
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    break;
+
+            }
+            determineWinner(playerTurn, computerTurn);
+        }
+        public void determineWinner(int player, int computer)
+        {
+            if (player == computer)
+            {
+                label3.Text = "Remis!";
+            }
+            else if ((player == 1 && computer == 3) || (player == 2 && computer == 1) || (player == 3 && computer == 2))
+            {
+                label3.Text = "Ufoludek wygrywa!";
+            }
+            else
+            {
+                label3.Text = "Ufoludek przegrywa!"; ///jeszcze dodanie utraty punktów i benzyny
+            }
         }
     }
 }
